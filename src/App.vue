4719ba090371fe19839e28d26f918a9fc4692401
hero7212 <2423467330@qq.com>
2020-05-22 11:21:33 +0800

    p04

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Add />
+
+    <p>----------------------------------------</p>
+
+    <Sub />
+  </div>
+</template>
+
+<script>
+import Add from '@/components/Add'
+import Sub from '@/components/Sub'
+export default {
+  components: {
+    Add,
+    Sub
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
